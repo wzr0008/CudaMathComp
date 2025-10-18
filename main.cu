0703965a002a43df59d41402004677c3ea2a87a5
aubur <wzr0008@auburn.edu>
2025-10-18 14:59:39 -0700

    Test Gpu

--- main.cu
+++ main.cu
@@ -1,7 +1,13 @@
 #include <iostream>
-
+__global__ void kernel();
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
+    kernel<<<1,100>>>();
+    cudaDeviceSynchronize();
     return 0;
+}
+__global__ void kernel()
+{
+    printf("Hello from Gpu\n");
 }
